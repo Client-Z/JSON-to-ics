497d1d31879bdd2f7a0dfd82a835b3c6c63c7c4b
Client-Z <denisoleshenko@gmail.com>
2017-02-14 16:39:12 +0200

    small changes

--- js/main.js
+++ js/main.js
@@ -1,64 +1,64 @@
 const beginICalendar = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//lanyrd.com//Lanyrd//EN\nX-ORIGINAL-URL:http://lanyrd.com/2016/xpdays/xpdays-schedule.ics\nX-WR-CALNAME;CHARSET=utf-8:XP Days Ukraine 2016 schedule\nMETHOD:PUBLISH\nX-MS-OLK-FORCEINSPECTOROPEN:TRUE\n";
 const endICalendar = "END:VCALENDAR";
 const maxLengthProperty = 70;
-let google_events = ""; // googleEvents (camel case)
+let googleEvents = "";
 let dtStamp = dtStampGenerate();
 
 var xhr = new XMLHttpRequest();
 xhr.onload = function(){
-	google_events = xhr.responseText;
-	let google_events_obj = JSON.parse(google_events);
-	google_events = beginICalendar;
-	for (let i = 0; i < google_events_obj.length; i++) {
-	 	google_events = eventToICalendar(google_events_obj[i], google_events);
+	googleEvents = xhr.responseText;
+	let googleEvents_obj = JSON.parse(googleEvents);
+	googleEvents = beginICalendar;
+	for (let i = 0; i < googleEvents_obj.length; i++) {
+	 	googleEvents = eventToICalendar(googleEvents_obj[i], googleEvents);
 	}
-	google_events += endICalendar;
+	googleEvents += endICalendar;
 	// c 9 по 15 строку нужно вынести в отдельную функцию 
-	download(google_events, "calendar", "ics");
+	download(googleEvents, "calendar", "ics");
 }
-xhr.open('GET', 'http://javascript.kiev.ua/attach/icalendar/google_events.json', true);
+xhr.open('GET', 'http://javascript.kiev.ua/attach/icalendar/googleEvents.json', true);
 xhr.send( );
 
-// возможно лучше использовать метод concat вместо +=? -- ЛУЧШЕ concat и массив! 
-function eventToICalendar(event, google_events){
-	// старайся не повторять код, например  + "\n" делаеться многократно, а это плохо
-	google_events += "BEGIN:VEVENT\n"; // вместо google_events -> googleEvents (camel case)
-	google_events += "TITLE:" + contentLinesRFC(event.title) + "\n"; // "\n" лучше вынести в переменную 
-	google_events += "CLASS:" + event.className[0] + "\n";
-	google_events += "SUMMARY:\n";
-	google_events += "LOCATION:" + contentLinesRFC(event.location) + "\n";
-	google_events += "URL:" + event.url + "\n";
-	google_events += "UID:" + event.id + "\n";
-	google_events += "DTSTAMP:" + dtStamp + "\n";
-	google_events += "DTSTART:" + cleaningDate(event.start) + "\n";
-	google_events += "DTEND:" + cleaningDate(event.end) + "\n";
-	google_events += "ALLDAY:" + event.allDay.toString() + "\n";
-	google_events += "ICONURL:" + event.iconUrl + "\n";
-	google_events += "DESCRIPTION:" + "\n";
-	google_events += "END:VEVENT\n";
-	return google_events;
+
+function eventToICalendar(event, googleEvents){
+	var newLine = "\n";
+	googleEvents += "BEGIN:VEVENT\n";
+	googleEvents += "TITLE:" + contentLinesRFC(event.title) + "\n";
+	googleEvents += "CLASS:" + event.className[0] + "\n";
+	googleEvents += "SUMMARY:\n";
+	googleEvents += "LOCATION:" + contentLinesRFC(event.location) + "\n";
+	googleEvents += "URL:" + event.url + "\n";
+	googleEvents += "UID:" + event.id + "\n";
+	googleEvents += "DTSTAMP:" + dtStamp + "\n";
+	googleEvents += "DTSTART:" + cleaningDate(event.start) + "\n";
+	googleEvents += "DTEND:" + cleaningDate(event.end) + "\n";
+	googleEvents += "ALLDAY:" + event.allDay.toString() + "\n";
+	googleEvents += "ICONURL:" + event.iconUrl + "\n";
+	googleEvents += "DESCRIPTION:" + "\n";
+	googleEvents += "END:VEVENT\n";
+	return googleEvents;
 }
 
 // Вычисляем вес строки в байтах
-// Очень страшная, не понятная функция, должен быть вариант лучше 
 String.prototype.byteLength = function(){
-   var str = this, length = str.length, count = 0, i = 0, ch = 0;
-   for(i; i < length; i++){
-     ch = str.charCodeAt(i);
-     if(ch <= 127){
-        count++;
-     }else if(ch <= 2047){
-        count += 2;
-     }else if(ch <= 65535){
-        count += 3;
-     }else if(ch <= 2097151){
-        count += 4;
-     }else if(ch <= 67108863){
-        count += 5;
-     }else{
-        count += 6;
-     }    
-  }
+	var byteCounts = [127, 2047, 65535, 2097151, 67108863];
+    var str = this, length = str.length, count = 0, i = 0, ch = 0;
+    for(i; i < length; i++){
+		ch = str.charCodeAt(i);
+		if(ch <= byteCounts[0]){
+		    count++;
+		}else if(ch <= byteCounts[1]){
+		    count += 2;
+		}else if(ch <= byteCounts[2]){
+		    count += 3;
+		}else if(ch <= byteCounts[3]){
+		    count += 4;
+		}else if(ch <= byteCounts[4]){
+		    count += 5;
+		}else{
+		    count += 6;
+		}    
+  	}
   return count;
 };
 
@@ -76,10 +76,7 @@ function contentLinesRFC(str){
 function cleaningDate(date){
 	date = date.slice(0, 10);
 	for (var i = 0; i < date.length; i++) {
-		date = date.replace(":", "");
-		date = date.replace("-", "");
-		date = date.replace("+", "");
-		// date.replace(/:|-|\+/g, "") заменяет три строчки 
+		date = date.replace(/:|-|\+/g, "");
 	}
 	return date;
 }
@@ -110,8 +107,8 @@ function download(data, filename, type) {
 	now.toISOString().replace(/-|:|\..*$/g, ""); 
  **/
 function dtStampGenerate(){
-	var d = new Date(); // лучше называть переменные осмысленно например now, вместо datestring -> dateString (camel case)
-	var datestring = d.getFullYear() + "" + ("0"+(d.getMonth()+1)).slice(-2) + ("0" + d.getDate()).slice(-2) + "T" // 
-	+ d.getHours() + "" + ("0"+(d.getMinutes()+1)).slice(-2) + "" + ("0"+(d.getSeconds()+1)).slice(-2);
-	return datestring;
+	var now = new Date();
+	var dateString = now.getFullYear() + "" + ("0"+(now.getMonth()+1)).slice(-2) + ("0" + now.getDate()).slice(-2) + "T"
+	+ now.getHours() + "" + ("0"+(now.getMinutes()+1)).slice(-2) + "" + ("0"+(now.getSeconds()+1)).slice(-2);
+	return dateString;
 }
